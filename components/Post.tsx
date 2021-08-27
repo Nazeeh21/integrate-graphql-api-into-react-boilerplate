27526d62c14849a29d4ab1ba7a630a4f8e0648a9
Nazeeh21 <nazeehvahora.786@gmail.com>
2021-08-27 22:43:37 +0530

    Fix ui bugs

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import TrashPng from '../public/trash.png';
-import EditPng from '../public/edit.png';
 import { useRouter } from 'next/dist/client/router';
 
 interface PostsProps {
@@ -11,20 +9,24 @@ interface PostsProps {
 }
 
 const Post: React.FC<PostsProps> = ({ id, title, text }) => {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <div className="Post">
-      <div className="title">{title}</div>
-      <div className="text">{text}</div>
-      <div className="buttons">
-        <button onClick={() => {
-          router.push(`/update-post/${id}`)
-        }}>
-          <img src='./edit.png' alt="Edit" />
+    <div className='Post'>
+      <div>
+        <div className='title'>{title}</div>
+        <div className='text'>{text}</div>
+      </div>
+      <div className='buttons'>
+        <button
+          onClick={() => {
+            router.push(`/update-post/${id}`);
+          }}
+        >
+          <img src='./edit.png' alt='Edit' />
         </button>
         <button onClick={() => {}}>
-          <img src='./trash.png' alt="Delete" />
+          <img src='./trash.png' alt='Delete' />
         </button>
       </div>
     </div>
